3f4e17571584fe0eda1c6de09da16efe80e738ce
aaron shivers <aaorn@moonmail.org>
2019-09-05 09:32:12 -0500

    removed case insensitivity

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const paragraphs = document.querySelectorAll('p')
 
 input.addEventListener('input', e => {
   const searchTerm = e.target.value
-  const regex = new RegExp(searchTerm, 'ig')
+  const regex = new RegExp(searchTerm, 'g')
   const replacement = `<span class="highlighted">${ searchTerm }</span>`
 
   for (const p of paragraphs) {
